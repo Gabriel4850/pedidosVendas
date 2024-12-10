121c5fce205dbb32c2f6193752f54bce063a37f5
Gabriel4850 <gabriel.mlima09@outlook.com>
2024-12-10 09:38:24 -0300

    Update PesquisaCliente.vb

--- Estudo/PesquisaCliente.vb
+++ Estudo/PesquisaCliente.vb
@@ -1,6 +1,6 @@
 ﻿Imports System.Data.SqlClient
 Public Class PesquisaCliente
-    Dim connectionString As String = "Server=PC-TNESCRITORIO\MSSQLSERVER02;Database=estudo;Integrated Security=True;"
+    Dim connectionString As String = "Server=NomeServidor;Database=estudo;Integrated Security=True;"
 
     ' Variável para armazenar o tipo do formulário
     Private TipoForm As String
